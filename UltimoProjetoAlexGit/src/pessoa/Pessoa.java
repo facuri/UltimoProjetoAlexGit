62a70addca45279dd847864350d3a88346a31bef
dalmo <mffacuri@terra.com.br>
2023-03-09 13:27:49 -0300

    Adicionando um novo atributo idade na Classe Pessoa

--- UltimoProjetoAlexGit/src/pessoa/Pessoa.java
+++ UltimoProjetoAlexGit/src/pessoa/Pessoa.java
@@ -4,6 +4,7 @@ public class Pessoa {
 	
 	private String nome;
 	private String sobrenome;
+	private int idade;
 
 	public String getNome() {
 		return nome;
@@ -19,6 +20,14 @@ public class Pessoa {
 
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 	
 	
