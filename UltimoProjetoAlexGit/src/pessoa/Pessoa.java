b4e742d32b82cd5695ad73fac7d51e1e3dc4b575
dalmo <mffacuri@terra.com.br>
2023-03-09 13:17:24 -0300

    Alteração e criação de  um novo atributo sobrenome na Classe Pessoa

--- UltimoProjetoAlexGit/src/pessoa/Pessoa.java
+++ UltimoProjetoAlexGit/src/pessoa/Pessoa.java
@@ -3,6 +3,7 @@ package pessoa;
 public class Pessoa {
 	
 	private String nome;
+	private String sobrenome;
 
 	public String getNome() {
 		return nome;
@@ -11,6 +12,16 @@ public class Pessoa {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
 	
 	
 
